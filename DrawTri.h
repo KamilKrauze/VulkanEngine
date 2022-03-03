c5481dd3bd582a54df81a73a95c54ea7d0c61643
Kamil Krauze <kamila.p.krauze@gmail.com>
2022-03-03 14:33:41 +0000

    Physical Devices and Queue Families

--- DrawTri.h
+++ DrawTri.h
@@ -9,12 +9,17 @@
 #include <stdexcept> 
 #include <iostream>
 #include <vector>
+#include <map>
+#include <optional>
 
 class DrawTri
 {
 private:
 	GLFWwindow* window;
 	VkInstance instance;
+	VkDevice device;
+
+	VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
 
 	const uint32_t WIDTH = 800;
 	const uint32_t HEIGHT = 600;
@@ -31,6 +36,14 @@ private:
 
 	VkDebugUtilsMessengerEXT debugMessenger;
 
+	struct QueueFamilyIndices {
+		std::optional<uint32_t> graphicsFamily;
+
+		bool isComplete() {
+			return graphicsFamily.has_value();
+		}
+	};
+
 	void initWindow() {
 		glfwInit();
 
@@ -44,6 +57,100 @@ private:
 	void initVulkan() {
 		createInstance();
 		setupDebugMessenger();
+		pickPhysicalDevice();
+		createLogicalDevice();
+	}
+
+	void pickPhysicalDevice()
+	{
+		uint32_t deviceCount = 0;
+
+		if (deviceCount == 0)
+		{
+			throw std::runtime_error("RUNTIME ERROR: Failed to find a graphics device with Vulkan support");
+		}
+
+		std::vector<VkPhysicalDevice> devices(deviceCount);
+		vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+		for (const auto& device : devices)
+		{
+			if (isDeviceSuitable(device))
+			{
+				physicalDevice = device;
+				break;
+			}
+		}
+		if (physicalDevice == VK_NULL_HANDLE)
+		{
+			throw std::runtime_error("RUNTIME ERROR: Failed to find suitable graphics device.");
+		}
+	}
+
+	bool isDeviceSuitable(VkPhysicalDevice device)
+	{
+		QueueFamilyIndices indices = findQueueFamilies(device);
+
+		return indices.isComplete();
+	}
+
+	void createLogicalDevice()
+	{
+		QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+
+		VkDeviceQueueCreateInfo queueCreateInfo{};
+		queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+		queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+		queueCreateInfo.queueCount = 1;
+		float queuePriority = 1.0f;
+		queueCreateInfo.pQueuePriorities = &queuePriority;
+
+		VkPhysicalDeviceFeatures deviceFeatures{};
+
+		VkDeviceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+
+		createInfo.pQueueCreateInfos = &queueCreateInfo;
+		createInfo.queueCreateInfoCount = 1;
+
+		createInfo.pEnabledFeatures = &deviceFeatures;
+
+		createInfo.enabledExtensionCount = 0;
+
+		if (enableValidationLayers) {
+			createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+			createInfo.ppEnabledLayerNames = validationLayers.data();
+		}
+		else {
+			createInfo.enabledLayerCount = 0;
+		}
+
+		if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create logical device!");
+		}
+	}
+
+	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+		QueueFamilyIndices indices;
+
+		uint32_t queueFamilyCount = 0;
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+		std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+		int i = 0;
+		for (const auto& queueFamily : queueFamilies) {
+			if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+				indices.graphicsFamily = i;
+
+			if (indices.isComplete())
+				break;
+
+			i++;
+		}
+
+		return indices;
 	}
 
 	void mainLoop() {
@@ -56,7 +163,8 @@ private:
 	
 	void cleanup() {
 		std::cout << "Cleaning up" << std::endl;
-
+		
+		vkDestroyDevice(device, nullptr);
 		if (enableValidationLayers) {
 			DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
 		}
@@ -138,8 +246,10 @@ private:
 			createInfo.pNext = nullptr;
 		}
 
+		std::cout << "Enabled Layers: " << createInfo.enabledLayerCount << std::endl << std::endl;
+
 		if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
-			throw std::runtime_error("failed to create instance!");
+			throw std::runtime_error("\tRUNTIME ERROR: Failed to create instance!");
 		}
 	}
 
@@ -237,7 +347,8 @@ private:
 	}
 
 public:
-	void run() {
+	void run(){
+
 		initWindow();
 		initVulkan();
 		mainLoop();
